e585383958cff724cf2de7e6648912eb510a1c0e
Emil <emil.lehikoinen@outlook.com>
2022-10-05 11:01:29 +0300

    maps api updated

--- projekti/FinalProject.js
+++ projekti/FinalProject.js
@@ -14,7 +14,7 @@ fetch(URL).then(data=> {
 })
 
 // Asetukset paikkatiedon hakua varten (valinnainen)
-const options = {
+/*const options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
@@ -47,4 +47,34 @@ function error(err) {
 }
 
 // Käynnistetään paikkatietojen haku
-navigator.geolocation.getCurrentPosition(success, error, options);
+navigator.geolocation.getCurrentPosition(success, error, options); */
+
+
+var map;
+var service;
+var infowindow;
+
+function initMap() {
+    var sydney = new google.maps.LatLng(-33.867, 151.195);
+
+    infowindow = new google.maps.InfoWindow();
+
+    map = new google.maps.Map(
+        document.getElementById('map'), {center: sydney, zoom: 15});
+
+    var request = {
+        query: 'Museum of Contemporary Art Australia',
+        fields: ['name', 'geometry'],
+    };
+
+    var service = new google.maps.places.PlacesService(map);
+
+    service.findPlaceFromQuery(request, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+            map.setCenter(results[0].geometry.location);
+        }
+    });
+}
