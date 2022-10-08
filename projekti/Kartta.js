2b76a8c744d2062e3cd6e5865880c40504fe0492
KimRautiainen <kim.rautiainen@hotmail.com>
2022-10-09 01:19:19 +0300

    Kommentit lisätty ja koodi formatoitu

--- projekti/Kartta.js
+++ projekti/Kartta.js
@@ -1,3 +1,4 @@
+// Luodaan kartta elementtiin, keskitetään kuva Helsinkiin.
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {
@@ -7,107 +8,63 @@ function initMap() {
         zoom: 6,
 
     });
-
-
-
-
+    // Luodaan infoikkuna
     infoWindow = new google.maps.InfoWindow();
 
-
-
-
+    // Lisätään Eventlistener kun käyttäjä clickaa buttonia.
     paikannaElem.addEventListener("click", () => {
-        // Try HTML5 geolocation.
-
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
                 (position) => {
-                    const pos = {
+                    const pos = { // Paikannetaan laitteen koordinaatit
                         lat: position.coords.latitude,
                         lng: position.coords.longitude,
-
                     };
-
-                    infoWindow.setPosition(pos);
+                    infoWindow.setPosition(pos); // Lisätään infoikkuna paikannettuihin koordinaatteihin
                     infoWindow.setContent("Olet tässä");
                     infoWindow.open(map);
                     map.setCenter(pos);
                     map.setZoom(12);
-                    var request = {
+
+
+                    let request = { // Tallennetaan muuttujaan Api kyselyn vaatimat parametrit
                         location: pos,
                         radius: 5000,
                         type: ['restaurant'],
-
-
                     };
-
-                    service = new google.maps.places.PlacesService(map);
-                    service.nearbySearch(request, callback);
-
-
+                    service = new google.maps.places.PlacesService(map); // Kutsutaan Googlemaps places Apia
+                    service.nearbySearch(request, callback); // Kutsutaan places apin nearbysearch osiota ja lähetetään vaadittavat parametrit
 
                     function callback(results, status) {
                         if (status == google.maps.places.PlacesServiceStatus.OK) {
 
-                            for (var i = 0; i < results.length; i++) {
-                                var place = results[i];
-
-
-
-
-                                createMarker(results[i].geometry.location, place.name, place);
-
-
-                                console.log(place);
-                                console.log(place.name);
-                                console.log(place.rating);
-                                console.log(place.user_ratings_total);
-                                console.log(place.price_level);
-                                console.log(place.opening_hours);
-                                console.log(place.reviews);
-                                console.log(place.place_id);
-
-
-
-
-                                //console.log(place.icon);
-                                //console.log(place.opening_hours);
-
-
-
-
+                            for (let i = 0; i < results.length; i++) { // Iteroidaan taulukko läpi for loopilla
+                                let place = results[i];
+                                createMarker(results[i].geometry.location, place.name, place); // Kutsutaan funktiota createmarker ja lähetetään parametreinä
+                                // paikan koordinaatit, paikan nimi, sekä koko taulukko....
                             }
                         }
                     }
 
-
-
-
-                    function createMarker(position, title, place) {
-                        if (merkki && merkki.setMap) {
+                    function createMarker(position, title, place) { // Luodaan markerit saatujen parametrien avulla
+                        if (merkki && merkki.setMap) { // Poistetaan vanhat merkit jos käyttäjä painaa uudestaan nappia
                             merkki.setMap(null);
                         }
 
-                        var merkki = new google.maps.Marker({
+                        var merkki = new google.maps.Marker({ // Annetaan markkerille koordinaatit,kartan osoite, animaatio sekä nimi
                             position: position,
                             map: map,
                             animation: google.maps.Animation.DROP,
                             title: title,
-
-
-
-
                         });
-
-
-
-                        google.maps.event.addListener(merkki, 'click', function() {
-
-
-
+                        google.maps.event.addListener(merkki, 'click', function() { // Lisätään markerille eventlisteneri jos käyttäjä clickkaa markkeria
                             map.setZoom(14);
                             map.setCenter(merkki.getPosition());
-                            infoWindow.setPosition(position);
+                            infoWindow.setPosition(position); // lisätään markkereille infoikkuna
+
+
+
+                            // Luodaan if konditio jos api kyselystä ei palaudukkaan kuvaa, jotta muut merkit toimivat
 
                             if (place.photos[0].getUrl({
                                 'maxWidth': 500,
@@ -127,12 +84,9 @@ function initMap() {
                                 <a></a>
                             </p>
                      </article>
-                  </div>   
-              
-              
-              
+                  </div>                              
               `)
-                                infoWindow.open(map, merkki);;
+                                infoWindow.open(map, merkki);
                             } else {
                                 infoWindow.setContent(`
                <div>
@@ -149,22 +103,11 @@ function initMap() {
                             </p>
                      </article>
                   </div>   
-              
-              
-              
               `)
-                                infoWindow.open(map, merkki);;
+                                infoWindow.open(map, merkki); // Avataan infoikkuna markkereiden luokse
                             }
-
-
-
-
                         });
-
-
-
                     }
-
                 },
                 () => {
                     handleLocationError(true, infoWindow, map.getCenter());
@@ -178,7 +121,6 @@ function initMap() {
 
 }
 
-
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(
@@ -189,7 +131,5 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.open(map);
     Results();
 }
-
 window.onload = initMap;
-
-const paikannaElem = document.getElementById("nayta");
+const paikannaElem = document.getElementById("nayta"); // Tallennetaan button elementti
